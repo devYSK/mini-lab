85dafa66a4a8f6210fbd6b6d4d1f6793b8780de7
devYSK <kim206gh@naver.com>
2025-02-10 20:39:15 +0900

    fix : boot version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
 	kotlin("kapt") version "1.9.25"
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 allprojects {
